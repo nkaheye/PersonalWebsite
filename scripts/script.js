e47fac3208dee93b0a686f1d9dec5432dea0dc3d
Kayeh Kayeh <nkayeh@my.bcit.ca>
2022-11-08 12:49:18 -0800

    create course array consisting of courses as objects

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+
+let ACIT1310 = {
+    code: "ACIT 1310",
+    name: "Technical Math for IT",
+}
+let ACIT1620 = {
+    code: "ACIT 1620",
+    name: "Fundamental Web Technologies",
+}
+let ACIT1515 = {
+    code: "ACIT 1515",
+    name: "Scripting for IT",
+}
+const courselist =[ACIT1515, ACIT1620, ACIT1310]
