6bb099427457c5e63cafcad86497e5a74bbee9f4
Kayeh Kayeh <nkayeh@my.bcit.ca>
2022-11-08 18:47:30 -0800

    add do while loop to check if input is 4 numbers

--- scripts/script.js
+++ scripts/script.js
@@ -11,4 +11,20 @@ let ACIT1515 = {
     code: "ACIT 1515",
     name: "Scripting for IT",
 }
-const courselist =[ACIT1515, ACIT1620, ACIT1310]
+const courselist =[ACIT1515, ACIT1620, ACIT1310];
+
+let input = "";
+let i = 0;
+do{
+    if(i === 0){
+    input = prompt("Enter course number: ");
+    }
+    else{
+    input = prompt("Note a valid entry, please enter course code: ");
+    }
+    i++
+}
+// check if input is number and is 4 characters long
+while(isNaN(Number(input)) || input.length != 4);
+
+
